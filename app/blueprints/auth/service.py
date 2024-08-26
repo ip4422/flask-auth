30720716cdc8903e7d6439c05ad1c5634f4f5f5e
Igor Popov <ipopov4422@gmail.com>
2024-08-26 10:47:39 +0100

    Extract messages

--- app/blueprints/auth/service.py
+++ app/blueprints/auth/service.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-from flask import request, jsonify
+from flask import abort, request, jsonify
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_restful import Resource
@@ -10,17 +10,20 @@ from flask_jwt_extended import (
 )
 from app.utils.response_utils import make_json_response
 from app.blueprints.user.model.users import User, user_list
+from app.utils.response_utils import ResponseMessage
 
 
 class Register(Resource):
     def post(self):
         if not request.is_json:
-            return make_json_response({'message': 'Missing JSON in request'}, HTTPStatus.BAD_REQUEST)
+            abort(HTTPStatus.BAD_REQUEST,
+                  ResponseMessage.INVALID_REQUEST_JSON_BODY)
 
         data = request.get_json()
         required_data = {"password", "username", "email"}
         if not all(key in data for key in required_data):
-            return make_json_response({'message': 'Missing data in request'}, HTTPStatus.BAD_REQUEST)
+            abort(HTTPStatus.BAD_REQUEST,
+                  ResponseMessage.INVALID_AUTH_REQUEST)
 
         hashed_password = data['password']
         hashed_password = generate_password_hash(
@@ -28,36 +31,39 @@ class Register(Resource):
         new_user = User(public_id=str(
             uuid.uuid4()), username=data['username'], email=data['email'], password=hashed_password)
         new_user.save()
-        return make_json_response({'message': 'User registered successfully!'}, HTTPStatus.CREATED)
+        return make_json_response({'message': ResponseMessage.USER_CREATED}, HTTPStatus.CREATED)
 
 
 class Login(Resource):
     def post(self):
         if not request.is_json:
-            return make_json_response({'message': 'Missing JSON in request'}, HTTPStatus.BAD_REQUEST)
+            abort(HTTPStatus.BAD_REQUEST,
+                  ResponseMessage.INVALID_REQUEST_JSON_BODY)
 
         data = request.get_json()
         required_data = {"password", "email"}
         if not all(key in data for key in required_data):
-            return make_json_response({'message': 'Missing data in request'}, HTTPStatus.BAD_REQUEST)
+            abort(HTTPStatus.BAD_REQUEST,
+                  ResponseMessage.INVALID_AUTH_REQUEST)
 
         user = User.get_by_email(data['email'])
         if not user:
-            return make_json_response({'message': 'User not found!'}, HTTPStatus.NOT_FOUND)
+            return make_json_response({'message': ResponseMessage.USER_NOT_FOUND}, HTTPStatus.NOT_FOUND)
+            # return make_json_response({'message': 'User not found!'}, HTTPStatus.NOT_FOUND)
         if user.check_password(data['password']):
             access_token = create_access_token(
                 identity=user.public_id, fresh=True)
             refresh_token = create_refresh_token(identity=user.public_id)
             return make_json_response({'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK)
-        return make_json_response({'message': 'Something wrong!'}, HTTPStatus.UNAUTHORIZED)
+        return make_json_response({'message': ResponseMessage.SOMETHING_WENT_WRONG}, HTTPStatus.UNAUTHORIZED)
 
 
 class Logout(Resource):
     @jwt_required()
     def delete(self):
-        resp = jsonify({"msg": "Successfully logged out"})
+        resp = jsonify({"msg": ResponseMessage.SUCCESSFULLY_LOGGED_OUT})
         unset_jwt_cookies(resp)
-        return make_json_response({'message': 'Logged out'}, HTTPStatus.OK)
+        return make_json_response({'message': ResponseMessage.LOGGED_OUT}, HTTPStatus.OK)
 
 
 class Profile(Resource):
@@ -65,12 +71,12 @@ class Profile(Resource):
     def get(self):
         current_user = get_jwt_identity()
         if not current_user:
-            return make_json_response({'message': 'You are not logged in!'}, HTTPStatus.UNAUTHORIZED)
+            return make_json_response({'message': ResponseMessage.NOT_LOGGED_IN}, HTTPStatus.UNAUTHORIZED)
         user = next(
             (user for user in user_list if user.public_id == current_user), None)
         if not user:
             make_json_response(
-                {'message': 'User not found!'}, HTTPStatus.NOT_FOUND)
+                {'message': ResponseMessage.USER_NOT_FOUND}, HTTPStatus.NOT_FOUND)
         user_data = {
             'public_id': user.public_id,
             'username': user.username,
--- app/utils/response_utils.py
+++ app/utils/response_utils.py
@@ -1,4 +1,24 @@
+from enum import StrEnum
 from flask import jsonify, make_response
+
+
+class ResponseMessage(StrEnum):
+    INVALID_REQUEST_JSON_BODY = "Missing JSON in request"
+    INVALID_AUTH_REQUEST = "Provided authentication data is invalid"
+
+    # User
+    USER_NOT_FOUND = "User not found"
+    USER_CREATED = "User created successfully"
+
+    # Auth
+    LOGGED_OUT = "Logged out"
+    SUCCESSFULLY_LOGGED_OUT = "Successfully logged out"
+    LOGGED_IN = "Logged in"
+    SUCCESSFULLY_LOGGED_IN = "Successfully logged in"
+    NOT_LOGGED_IN = "You are not logged in!"
+
+    # Common
+    SOMETHING_WENT_WRONG = "Something went wrong"
 
 
 def make_json_response(data, status, headers=None):
